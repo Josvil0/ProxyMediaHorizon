6741148c1178e2427791d3902e680d16abfc1541
Josvil0 <156458754+Josvil0@users.noreply.github.com>
2025-05-27 19:20:46 +0200

    aun mas pruebas

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,3 @@
-export const config = {
-  runtime: 'nodejs',
-};
-
 export default async function handler(req, res) {
   const { url } = req.query;
   if (!url) return res.status(400).json({ error: 'Missing url param' });
